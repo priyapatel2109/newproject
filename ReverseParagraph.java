e0663f93928a3e588dcdfd251d366523e359a43f
Priya Patel <70574440+priyapatel2109@users.noreply.github.com>
2020-09-03 11:49:19 +0530

    Update ReverseParagraph.java

--- ReverseParagraph.java
+++ ReverseParagraph.java
@@ -3,12 +3,12 @@ public class ReverseParagraph
 {
 	public static void main(String args[])
 	{
-		//user input
-		Scanner sc= new Scanner(System.in);
-		System.out.print("Enter a paragraph : ");
-		String name= sc.nextLine();
-		
-		//getting words till .(dot)
+		//user input 
+		//Scanner sc= new Scanner(System.in);
+		//System.out.print("Enter a paragraph : ");
+		//String name= sc.nextLine();
+		String name =  "This is a coding test. I like java so much. This is my day.";
+	
 		String[] para = name.split("\\.");
 		
 		for (int i = 0 ; i <= para.length -1; i++)
@@ -17,7 +17,7 @@ public class ReverseParagraph
 			result0 = para[i];
 			
 			String[] words = result0.split(" ");
-			//for reverse words
+	
 			String result1 = "";
 			for (int j = words.length - 3; j >= 0; j--)
 			{
@@ -25,7 +25,7 @@ public class ReverseParagraph
 			}
 			System.out.print(result1.trim());
 			String result2 = "";
-			//for remaining last 2 words
+	
 			for (int k = words.length - 2; k <= words.length - 1 ; k++)
 			{
 				result2 +=	(" " + words[k]);
